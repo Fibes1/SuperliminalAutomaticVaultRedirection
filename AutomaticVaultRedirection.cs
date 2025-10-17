f849e678855a79e50bab9eda6023b0dce9973f37
Fibes <75185623+Fibes1@users.noreply.github.com>
2025-10-17 16:58:58 +0100

    Add files via upload

---
+++ AutomaticVaultRedirection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+
+namespace AutomaticVaultRedirection
+{
+    public class AutomaticVaultRedirection : MelonMod
+    {
+        public static float rotation = 0f;
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                rotation = 270f;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                rotation = 90f;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                rotation = 180f;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha0))
+            {
+                rotation = 0f;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerLerpMantle), "LerpPlayer")]
+    public static class Patch
+    {
+        private static void Prefix()
+        {
+            GameManager.GM.player.GetComponent<CharacterMotor>().transform.Rotate(new Vector3(0f, AutomaticVaultRedirection.rotation, 0f));
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using AutomaticVaultRedirection;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(AutomaticVaultRedirection.AutomaticVaultRedirection), "Automatic Vault Redirection", "0.1.0", "Fibes")]
+[assembly: MelonGame("PillowCastle", "Superliminal")]
+[assembly: MelonGame("PillowCastle", "SuperliminalSteam")]
+[assembly: MelonGame("PillowCastle", "SuperliminalGOG")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutomaticVaultRedirection")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutomaticVaultRedirection")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4b01371d-6639-4042-9648-0da2c0b82260")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
